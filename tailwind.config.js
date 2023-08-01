a7e357d84dfd774627461ed9bacc0e403214632f
James Brooks <james@alt-three.com>
2023-08-01 15:39:43 +0100

    Use @hempworks/pilgrim theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
-import config from './node_modules/pilgrim-theme/tailwind.config.js'
+import config from '@hempworks/pilgrim/tailwind'
 
 const primary = {
 //   50: '#f2f9ff',
@@ -18,7 +18,7 @@ const primary = {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [
-    require('./node_modules/pilgrim-theme/tailwind.config.js')
+    require('@hempworks/pilgrim/tailwind')
   ],
   
   content: [
